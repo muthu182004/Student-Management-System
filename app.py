71e96d9fc31ccac7cabcd62802241895faa0b74c
MuthuKumar <muthukumar182004@gmail.com>
2026-06-17 21:39:39 +0530

    Added DB_PORT

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ TABLE_NAME = "student_info"
 def get_db_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
